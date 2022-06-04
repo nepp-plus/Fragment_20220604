b7747c4d1142aa3b6af8194bfee8ab7f1ecbf402
조경진 <cho881020@gmail.com>
2022-06-04 16:06:40 +0900

    프래그먼트의 버튼이 눌리면 > 로그 찍도로 이벤트 처리

--- app/src/main/java/com/neppplus/fragment_20220604/fragments/LogFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20220604/fragments/LogFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.fragment_20220604.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20220604.R
+import kotlinx.android.synthetic.main.fragment_log.*
 
 class LogFragment : Fragment() {
 
@@ -19,6 +21,17 @@ class LogFragment : Fragment() {
 
 //        fragment_log를 이 클래스의 내용물로 설정.
         return inflater.inflate( R.layout.fragment_log, container, false )
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnLogInFragment.setOnClickListener {
+
+            Log.d("로그프래그먼트", "프래그먼트에서 실행되는 로그 코드")
+
+        }
 
     }
 
